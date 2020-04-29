2c07a3c7eb209a34a17f1622888307f08378c0c1
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-29 18:17:16 +0000

    tired

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ Init for views API Blueprint
 
 from flask import Blueprint
 
-app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+app_views = Blueprint('api_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.cities import *
