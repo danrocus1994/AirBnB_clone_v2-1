c380995f5095997b4ff34c530b99f3388842464f
Viiic98 <xathovic@gmail.com>
2020-04-29 13:31:29 -0500

    Add task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,9 +4,9 @@ Init for views API Blueprint
 """
 
 
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
 from flask import Blueprint
 
 app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-from api.v1.views.cities import *
---
+++ api/v1/views/states.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+States route for AirBnB clone v3 API v1
+"""
+
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+from flask import jsonify, make_response, abort, request
+import json
+
+
+@app_views.route('/states', methods=['GET'])
+def states():
+    """
+    This route retrieves the list of all State objects
+    """
+    states = storage.all(State)
+    states_list = []
+    for state in states.values():
+        states_list.append(state.to_dict())
+    return jsonify(states_list), 200
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def state_by_id(state_id):
+    """
+    This route retrieves a State object
+    @state_id: id of State
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify(error="Not found"), 404
+    return jsonify(state.to_dict()), 200
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def del_state(state_id):
+    """
+    This route delete a state
+    @state_id: id of State that will be deleted
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify(error="Not found"), 404
+    storage.delete(state)
+    storage.save()
+    return {}, 200
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """
+    This route create a new state
+    Require at least name
+    """
+    if request.is_json:
+        req = request.get_json()
+        if req['name']:
+            new_state = State(**req)
+            storage.new(new_state)
+            storage.save()
+            return jsonify(new_state.to_dict()), 201
+        else:
+            return jsonify(error="Missing name"), 400
+    return jsonify(error="Not a JSON"), 400
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """
+    This route update a state
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify(error="Not found"), 404
+    if request.is_json is None:
+        return jsonify(error="Not a JSON"), 400
+    req = request.get_json()
+    for key, value in req.items():
+        if key != 'id' or key != 'created_at' or key != 'updated_at':
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
