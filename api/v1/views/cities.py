207cb931e030b9ed10e5bed12830a33a2674a578
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-29 15:22:50 +0000

    route states/<id>/cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,3 +10,23 @@ from models.city import City
 from api.v1.views import app_views
 from flask import jsonify, make_response, abort, request
 import json
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
+def cities(state_id):
+    """
+    This route return a list of cities given by the status id
+    """
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify(error="Not found"), 404
+    cities = storage.all(City)
+    cities_list = []
+    for key, city in cities.items():
+        if city.state_id == state_id:
+            cities_list.append(city.to_dict())
+    response = make_response(json.dumps(cities_list,
+                                        indent=2,
+                                        sort_keys=True), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
