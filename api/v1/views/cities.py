2fb11973990610eeaea80a5c6b0dc5aeb89834b0
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-29 05:40:15 +0000

    checking state_id

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -25,7 +25,5 @@ def cities(state_id):
     for key, city in cities.items():
         if city.state_id == state_id:
             cities_list.append(city.to_dict())
-    if len(cities_list) == 0:
-        abort(404)
     return Response(json.dumps(cities_list, indent=2, sort_keys=True), 200,
                     mimetype='application/json')
